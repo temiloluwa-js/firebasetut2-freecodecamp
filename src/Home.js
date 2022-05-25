2edcd633e9f13735110177109eea75f110253a68
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-05-25 07:02:48 +0100

    update docs

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { getDocs, collection, updateDoc, doc } from "firebase/firestore";
 import { database } from "./firebase-config";
-import { getDocs, collection, doc, updateDoc } from "firebase/firestore";
 const Home = () => {
   const collectionRef = collection(database, "users");
+
+  //get data from database
   const getData = () => {
     getDocs(collectionRef).then((response) => {
       response.docs.map((item) => {
@@ -12,9 +14,18 @@ const Home = () => {
     });
   };
 
+  const updateData = () => {
+    const doctoUpdate = doc(database, "users", "PnYjY7KA8myu1zjJAGgi");
+    updateDoc(doctoUpdate, {
+      email: "ade@b.com",
+      password: "akin@a.com"
+    })
+  };
+
   return (
     <div>
-      <button onClick={getData}></button>
+      <button onClick={getData}>Get Data</button>
+      <button onClick={updateData}>Update Data</button>
     </div>
   );
 };
--- src/Register.js
+++ src/Register.js
@@ -35,3 +35,4 @@ const Register = () => {
 };
 
 export default Register;
+ 
